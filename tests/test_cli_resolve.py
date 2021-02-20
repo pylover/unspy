68ecc98612db905dd68411e0f978227c0007fc91
pylover <vahid.mardani@gmail.com>
2021-02-20 21:57:19 +0330

    Test resolve short result

--- tests/test_cli_resolve.py
+++ tests/test_cli_resolve.py
@@ -1,9 +1,27 @@
+from unittest import mock
+
+
 def test_cli_resolve(socketclass_mock, cliapp):
-    status, stdout, stderr = cliapp('resolve', 'foo.com')
-    assert stderr == ''
-    assert status == 0
-    assert stdout == '10.0.0.2 foo.com\n'
+    s, o, e = cliapp('resolve', 'foo.com')
+    assert e == ''
+    assert s == 0
+    assert o == '10.0.0.2 foo.com\n'
     socketclass_mock.assert_called_once()
+    sock = socketclass_mock.return_value
+    sock.settimeout.assert_not_called()
+    sock.sendto.assert_called_once_with(b'\x01foo.com', mock.ANY)
+    sock.recvfrom.assert_called_once()
+
+    # Missing argument
+    s, o, e = cliapp('resolve')
+    assert e.startswith('usage: uns resolve [-h]')
+    assert s == 1
+    assert o == ''
+
+    # Short result
+    s, o, e = cliapp('resolve', '-s', 'foo.com')
+    assert s == 0
+    assert o == '10.0.0.2\n'
 
 
 if __name__ == '__main__':
