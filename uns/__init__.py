38a4dd5b70574992f21413df9e05a4ef7a06a9e7
pylover <vahid.mardani@gmail.com>
2021-03-08 23:07:22 +0330

    v1.4.0

--- uns/__init__.py
+++ uns/__init__.py
@@ -6,7 +6,7 @@ from . import protocol, cache
 from .constants import DEFAULT_DBFILE
 
 
-__version__ = '1.3.1'
+__version__ = '1.4.0'
 
 
 def resolve(hostname, timeout=0, force=False, noresolve=False,
